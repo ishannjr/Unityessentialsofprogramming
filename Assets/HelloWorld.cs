61c3788eb64e41e7c6c289642f7f8ec12da83fae
ishannjr <ishaan.mandliya@somaiya.edu>
2024-02-08 22:57:34 -0800

    commit

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public string myMessage;
+    void Start()
+    {
+
+
+    }
+
+   
+    void Update()
+    {
+        Debug.Log(myMessage);
+
+    }
+}
---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,74 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247 (void);
+extern void HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5 (void);
+extern void HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35 (void);
+extern void negativeposition_Start_mC0B7257714C6A26AFC00905593588810A60CE6FD (void);
+extern void negativeposition_Update_m5B0162B2D5EFE0C0E142EC17E008BD88F0A3FEAB (void);
+extern void negativeposition__ctor_mF8A70DED0FBC7E76482143DB959F23CF1551C63E (void);
+extern void scale_Start_mA7D5C387E8555160F95FF045CBACEFEB796424D9 (void);
+extern void scale_Update_m46FBFEC1FB1E6628B571D6CDC4E409FEA79007C7 (void);
+extern void scale__ctor_m5A2930E4B20B39E605EA84CD789BB1AFA9479B16 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247,
+	HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5,
+	HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35,
+	negativeposition_Start_mC0B7257714C6A26AFC00905593588810A60CE6FD,
+	negativeposition_Update_m5B0162B2D5EFE0C0E142EC17E008BD88F0A3FEAB,
+	negativeposition__ctor_mF8A70DED0FBC7E76482143DB959F23CF1551C63E,
+	scale_Start_mA7D5C387E8555160F95FF045CBACEFEB796424D9,
+	scale_Update_m46FBFEC1FB1E6628B571D6CDC4E409FEA79007C7,
+	scale__ctor_m5A2930E4B20B39E605EA84CD789BB1AFA9479B16,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	2139,
+	1112,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
